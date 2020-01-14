f6e13fa2853172d1a6a13a2ddaad33938f13a153
Alvaro Alvarez <alvaro.alvarez@2transfair.com>
2020-01-14 13:42:54 -0500

    adding scopes to posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  scope :published, -> { where.not(:published_at => nil)}
+  scope :unpublished, -> { where(:published_at => nil)}
+
+  # Ex:- scope :active, -> {where(:active => true)}
 end
